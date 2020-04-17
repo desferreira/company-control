b46063f6546886ee7c801cd411a505a7630c4614
desferreira <diego.ferreira@justa.com.vc>
2020-04-16 22:54:09 -0300

    :construction: Estrutura inicial do projeto com springboot
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

---
+++ src/main/java/com/diego/companycontrol/CompanycontrolApplication.java
@@ -0,0 +1,13 @@
+package com.diego.companycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanycontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanycontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/companycontrol/CompanycontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.companycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanycontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
