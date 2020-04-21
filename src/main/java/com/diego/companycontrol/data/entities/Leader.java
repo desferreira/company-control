c3f2632404909efc677e23757ab67c0fd52c5555
desferreira <diego.ferreira@justa.com.vc>
2020-04-21 19:54:05 -0300

    :arrow_up: Adiciona dependencias para PostgreSQL e profiles da aplicação
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

--- src/main/java/com/diego/companycontrol/data/entities/Leader.java
+++ src/main/java/com/diego/companycontrol/data/entities/Leader.java
@@ -4,11 +4,8 @@ import com.diego.companycontrol.data.entities.enums.EmployeeRole;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import java.time.LocalDate;
 import java.util.Date;
 
-@Table(name = "employee")
 @Entity
 @NoArgsConstructor
 public class Leader extends Employee {
